cb2785a3d6a759b9df79beba797a6d7464c21017
studioshader2018@gmail.com <studioshader2018@gmail.com>
2021-03-10 15:32:52 +0300

    +IntList +Tests

--- 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
+++ 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
@@ -1,21 +1,102 @@
 package org.spbsu.mkn.scala
 
+import org.graalvm.compiler.core.common.util.IntList
 import org.spbsu.mkn.scala.IntList._
 
-sealed trait IntList {
-  def head: Int
-  def tail: IntList
-  def drop(n: Int): IntList
-  def take(n: Int): IntList
-  def map(f: Int => Int): IntList
-  def ::(elem: Int): IntList = ???
+import scala.annotation.tailrec
+
+sealed trait List[+A]{
+  def head: A
+
+  def tail: List[A]
+
+  def drop(n: Int): List[A]
+
+  def take(n: Int): List[A]
+
+  def map[B](f: A => B): List[B]
+
+  //def ::(elem: A): List[A]
+}
+
+case class FPList[A](head: A, tail: List[A]) extends List[A] {
+
+  override def drop(n: Int): List[A] = {
+    if (n <= 0) {
+      return this
+    }
+    tail.drop(n - 1)
+  }
+
+  override def take(n: Int): List[A] = {
+    if (n <= 0) {
+      return FPNil
+    }
+    FPList[A](head, tail.take(n-1))
+  }
+
+  override def map[B](f: A => B): List[B] = {
+    FPList[B](f(head), tail.map(f))
+  }
+
+//  override def ::(elem: A): List[A] = {
+//    FPList[A](elem, this)
+//  }
+}
+
+case object FPNil extends List[Nothing] {
+  override def head: Nothing = {
+    throw new UnsupportedOperationException("trying to take non existing element")
+  }
+
+  override def tail: List[Nothing] = {
+    throw new UnsupportedOperationException("trying to take non existing element")
+  }
+
+  override def drop(n: Int): List[Nothing] = {
+    if (n != 0) {
+      throw new UnsupportedOperationException("trying to drop more elements than exists")
+    }
+    this
+  }
+
+  override def take(n: Int): List[Nothing] = {
+    if (n > 0) {
+      throw new UnsupportedOperationException("trying to take more elements than exists")
+    }
+    this
+  }
+
+  override def map[B](f: Nothing => B): List[B] = {
+    this
+  }
+
+//  override def ::(elem: _): List[_] = {
+//    this
+//  }
 }
 
 object IntList {
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): IntList = ???
-  def sum(intList: IntList): Int      = ???
-  def size(intList: IntList): Int     = ???
-  // extra task: implement sum using foldLeft
-  // def foldLeft(???)(???): ??? = ???
+
+  def fromSeq[A](seq: Seq[A]): List[A] = {
+    if (seq.isEmpty) {
+      return FPNil
+    }
+    FPList(seq.head, fromSeq(seq.tail))
+  }
+
+  def sum[A](list: List[A]): A = ???
+//  def sum(list: List[A]): A = {
+//    if (intList.equals(FPNil)) throw new UnsupportedOperationException("idk why")
+//    foldLeft((a, b) => a + b, 0, intList)
+//  }
+
+  def size[A](list: List[A]): Int = foldLeft[A, Int]((a, _) => a + 1, 0, list)
+
+  @tailrec
+  def foldLeft[A, B](f: (B, A) => B, start: B, list: List[A]): B = {
+    if (list.equals(FPNil)) return start
+    foldLeft(f, f(start, list.head), list.tail)
+  }
 }
--- 02/practice/src/test/scala/org/spbsu/mkn/scala/IntListTest.scala
+++ 02/practice/src/test/scala/org/spbsu/mkn/scala/IntListTest.scala
@@ -4,46 +4,77 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.spbsu.mkn.scala.IntList.{fromSeq, size, sum}
 
 class IntListTest extends AnyFunSuite {
+
+  test("construct"){
+    assert(size(FPList("Some", fromSeq(Seq(1,2,3))))==4)
+    assert(FPList("Some", fromSeq(Seq(1,2,3))).head=="Some")
+    assert(FPList("Some", fromSeq(Seq(1,2,3))).take(2) == FPList("Some",FPList(1,FPNil)))
+    assert(FPList("Some", fromSeq(Seq(1))).map(_ + "s") == FPList("Somes",FPList("1s",FPNil)))
+    assert(FPList("Some", fromSeq(Seq(Some(1)))).map(_ + "s") == FPList("Somes",FPList("Some(1)s",FPNil)))
+    // i don't really understand why this is happening, but i guess this could be interpreted as a correct solution
+  }
+
   test("head") {
     assert(fromSeq(Seq(1,2,3)).head == 1)
     assert(fromSeq(Seq(1)).head == 1)
     assertThrows[UnsupportedOperationException](fromSeq(Seq()).head)
+
+    // String
+    assert(fromSeq(Seq("ab","cd","ef")).head == "ab")
+    assert(fromSeq(Seq("some")).head == "some")
+    //assertThrows[UnsupportedOperationException](fromSeq(Seq()).head)
   }
 
   test("tail") {
     assert(fromSeq(Seq(1,2,3)).tail == fromSeq(Seq(2,3)))
-    assert(fromSeq(Seq(1)).tail == IntNil)
+    assert(fromSeq(Seq(1)).tail == FPNil)
+
+    assert(fromSeq(Seq("ab","cd","ef")).tail == fromSeq(Seq("cd","ef")))
+    assert(fromSeq(Seq("some")).tail == FPNil)
   }
 
   test("drop") {
     assert(fromSeq(Seq(1,2,3)).drop(0) == fromSeq(Seq(1,2,3)))
     assert(fromSeq(Seq(1,2,3)).drop(2) == fromSeq(Seq(3)))
-    assert(fromSeq(Seq(1,2,3)).drop(3) == IntNil)
+    assert(fromSeq(Seq(1,2,3)).drop(3) == FPNil)
     assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).drop(10))
+
+    assert(fromSeq(Seq("ab","cd","ef")).drop(0) == fromSeq(Seq("ab","cd","ef")))
+    assert(fromSeq(Seq("ab","cd","ef")).drop(2) == fromSeq(Seq("ef")))
+    assert(fromSeq(Seq("ab","cd","ef")).drop(3) == FPNil)
   }
 
   test("take") {
-    assert(fromSeq(Seq(1,2,3)).take(0) == IntNil)
+    assert(fromSeq(Seq(1,2,3)).take(0) == FPNil)
     assert(fromSeq(Seq(1,2,3)).take(2) == fromSeq(Seq(1,2)))
     assert(fromSeq(Seq(1,2,3)).take(3) == fromSeq(Seq(1,2,3)))
     assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).take(10))
+
+    assert(fromSeq(Seq("ab","cd","ef")).take(0) == FPNil)
+    assert(fromSeq(Seq("ab","cd","ef")).take(2) == fromSeq(Seq("ab","cd")))
+    assert(fromSeq(Seq("ab","cd","ef")).take(3) == fromSeq(Seq("ab","cd","ef")))
   }
 
   test("map") {
-    assert(IntNil.map(_ * 2) == IntNil)
+    assert(FPNil.map((_: Int) * 2) == FPNil)
     assert(fromSeq(Seq(1,2,3)).map(_ * 2) == fromSeq(Seq(2,4,6)))
     assert(fromSeq(Seq(1,2,3)).map(identity) == fromSeq(Seq(1,2,3)))
+
+    assert(FPNil.map((_: String) + "some") == FPNil)
+    assert(fromSeq(Seq("ab","cd","ef")).map(_ + "123") == fromSeq(Seq("ab123","cd123","ef123")))
+    assert(fromSeq(Seq("ab","cd","ef")).map(identity) == fromSeq(Seq("ab","cd","ef")))
   }
 
   test("size") {
-    assert(size(IntNil) == 0)
+    assert(size(FPNil) == 0)
     assert(size(fromSeq(Seq(1,2,3))) == 3)
+    assert(size(fromSeq(Seq("ab","cd","ef"))) == 3)
   }
 
-  test("sum") {
-    assertThrows[UnsupportedOperationException](sum(IntNil))
-    assert(sum(fromSeq(Seq(1,2,3))) == 6)
-    assert(sum(fromSeq(Seq(1))) == 1)
-  }
+//  test("sum") {
+//    assertThrows[UnsupportedOperationException](sum(FPNil))
+//    assert(sum(fromSeq(Seq(1,2,3))) == 6)
+//    assert(sum(fromSeq(Seq(1))) == 1)
+//  }
 
 }
